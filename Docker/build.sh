3d21144360b2d40dee697bbec6c454a0235b640c
Jordi Esteban <jesteban@iris.cat>
2017-07-03 12:53:52 +0200

    Add Cassandra to build script

--- Docker/build.sh
+++ Docker/build.sh
@@ -5,4 +5,5 @@ set -ex
 docker build -t="cm/cm-grafana" grafana
 docker build -t="cm/cm-prometheus" prometheus 
 docker build -t="cm/cm-cadvisor" cadvisor
+docker build -t="cm/cm-cassandra" cassandra
 docker build -t="cm/cm-node-exporter" prometheus/node-exporter
