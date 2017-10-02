8aadf8e020aa8aab8faa9427666b04d1b46226ae
Jordi Esteban <jesteban@iris.cat>
2017-10-02 19:37:35 +0200

    Update build script

--- Docker/build.sh
+++ Docker/build.sh
@@ -4,6 +4,8 @@ set -ex
 
 docker build -t="cm/cm-grafana" grafana
 docker build -t="cm/cm-prometheus" prometheus 
+docker build -t="cm/cm-alertmanager" alertmanager 
 docker build -t="cm/cm-cadvisor" cadvisor
 docker build -t="cm/cm-cassandra" cassandra
 docker build -t="cm/cm-node-exporter" prometheus/node-exporter
+docker build -t="cm/cm-api-test" api-test
